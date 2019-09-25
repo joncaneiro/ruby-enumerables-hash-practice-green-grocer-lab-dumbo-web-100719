aeec07d6b913c9c50d48d5a5ac709ea6d9081ee7
Jon Caneiro <joncaneiro@gmail.com>
2019-09-25 18:11:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,6 +29,7 @@ def apply_coupons(cart, coupons)
     
   item = coupon[:item] 
   
+  if cart[item]
     
   if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
     
@@ -47,7 +48,7 @@ def apply_coupons(cart, coupons)
          
      end
    end
-   
+  end 
    return cart
    
   end
