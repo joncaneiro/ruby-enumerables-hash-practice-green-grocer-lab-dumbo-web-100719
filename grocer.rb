4059fc8c8b7776c403eb2eb094f83511ee8fbc64
Jon Caneiro <joncaneiro@gmail.com>
2019-09-25 20:58:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,7 +72,7 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   
-  total = applied_discount.reduce(0)  { |acc, (key, value) | binding.pry} 
+  total = applied_discount.reduce(0)  { |acc, (key, value) | acc += } 
   
   
 
