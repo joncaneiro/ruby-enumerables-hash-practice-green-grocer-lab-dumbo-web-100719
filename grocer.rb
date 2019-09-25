9ee3c0e6dc170ce524b16934a92c7759d96f3601
Jon Caneiro <joncaneiro@gmail.com>
2019-09-25 20:28:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,7 +70,7 @@ def checkout(cart, coupons)
   
   hash_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(hash_cart, coupons)
-  
+  applied_clearance = apply_clearance(cart)
   
   
   
