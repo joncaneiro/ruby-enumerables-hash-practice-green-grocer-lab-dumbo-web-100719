7deb1ebb1765063afe500de3460d7d46838f4926
Jon Caneiro <joncaneiro@gmail.com>
2019-09-25 19:38:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -63,7 +63,7 @@ def apply_clearance(cart)
       
       if cart[item] && [item][:clearance] == TRUE
         
-        cart[item][:price] / 0.20 
+         clearance_price = cart[item][:price] / 0.20 
       
       eslif 
   
