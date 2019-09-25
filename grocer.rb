2095777626329fb413c1ac4f4a37f42e252b1c24
Jon Caneiro <joncaneiro@gmail.com>
2019-09-25 17:37:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,7 +33,7 @@ def apply_coupons(cart, coupons)
     
   if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
     
-     cart["#{item} W/COUPON"] = {:price => coupon[:cost] / coupon[:num], 
+     cart[" #{item} W/COUPON"] = {:price => coupon[:cost] / coupon[:num], 
                                  :clearance => cart[item][:clearance],
                                  :count => coupon[:num]
                                 }
